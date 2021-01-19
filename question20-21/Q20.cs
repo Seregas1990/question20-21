d4ae89eb213dd3c67fe296c5ce0220d094ce297c
Serhii Sierkov <Serhii Sierkov@DESKTOP-EOONJG7>
2021-01-18 19:51:57 -0500

    Add project files.

---
+++ question20-21/Q20.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace question20_21
+{
+    public class Q20 <T> where T : struct
+    {
+        private List<T> list = new List<T>();
+            public void Add (T newValue)
+        {
+            list.Add(newValue);
+        }
+        public T Return(int searchIdex)
+        {
+            return list[searchIdex];
+        }
+        public List<T> ReturnSortedCollection()
+        {
+            var ret = list;
+            return ret.OrderByDescending(l=>l).ToList();
+        }
+    }
+}
---
+++ question20-21/Q21.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace question20_21
+{
+    public delegate void EventHandler(string a);
+
+    public class Operation
+    {
+        public event EventHandler xyz;
+
+        public void Action(string a)
+        {
+            if (xyz != null)
+            {
+                xyz(a);
+                Console.WriteLine(a);
+            }
+            else
+            {
+                Console.WriteLine("Not Registered");
+            }
+        }
+    }
+
+    public class HandleOperation 
+    {
+        private static void CatchEvent(string s)
+        {
+            Console.WriteLine("Method Calling");
+        }
+        public void Handle(Operation o)
+        {
+            o.Action("Event Calling");
+            o.xyz += new EventHandler(CatchEvent);
+        }
+    }
+}
